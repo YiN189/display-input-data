321e27aa8b02d959759a623b500802b74c811612
Nyi Phyo Kyaw <me@npkk.dev>
2025-12-26 11:33:31 +0700

    react form

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+   base: '/display-input-data/',
   plugins: [react()],
-  base: '/display-input-data/'
 })
